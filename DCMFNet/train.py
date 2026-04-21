f424791ee68aaf6219fd9b78042cd2f4b84c741f
Silpa Soni Nallacheruvu <silnal@meb.ki.se>
2026-04-21 12:46:17 +0200

    epochs changed to 15

--- DCMFNet/train.py
+++ DCMFNet/train.py
@@ -32,7 +32,7 @@ import re
 # Hyperparameters
 learning_rate = 1.82e-4
 batch_size = 8
-num_epochs = 20
+num_epochs = 15
 
 # Add regularization techniques such as dropout and weight decay to prevent overfitting, especially given the small batch size and number of epochs.
 weight_decay = 1e-3  # Example weight decay for L2 regularization
